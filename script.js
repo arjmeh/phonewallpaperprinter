4b71b7dcbac573d7f61fc0bb14a740d21533b1cc
Arjun Mehrotra <arjun@arjuns-mbp.lan>
2023-10-16 20:14:47 -0700

    notfinished

--- script.js
+++ script.js
@@ -4,6 +4,38 @@ let isDragging = false;
 let offsetX, offsetY;
 let selectedFile; // Declare selectedFile as a global variable
 let imageUrl;
+let width = '';
+let height = '';
+
+
+
+const iphoneModels = {
+    'iphone13': { width: '270px', height: '555px',radius: '47.33pt' },
+    'iphone12': { width: '270px', height: '555px', radius: '47.33pt' },
+    'iphone11': { width: '250px', height: '545px',radius: '41.5pt' },
+    // Add more models as needed
+  };
+
+
+  function setModelDimensions(model) {
+    // Check if the model exists in the dictionary
+    if (model in iphoneModels) {
+      const { width, height, radius } = iphoneModels[model];
+      // Set the global variables
+      window.width = width;
+      window.height = height;
+      window.radius = radius;
+      phoneMockup.style.width = width;
+      phoneMockup.style.height = height;
+      console.log(window.width, window.height, window.radius)
+    } else {
+      // Model not found, handle the error as needed
+      window.width = '0px';
+      window.height = '0px';
+      window.radius = '0pt';
+    }
+  }
+
 
 // Function to disable or hide the file input
 function disableFileInput() {
@@ -64,14 +96,43 @@ function printImage() {
     if (selectedFile) {
         // Create an image element to hold the uploaded image
         var img = new Image();
+        img.onload = () => {
+            // create a canvas that will present the output image
+            const outputImage = document.createElement('canvas');
+        
+            // set it to the same size as the image
+            outputImage.width = img.naturalWidth;
+            outputImage.height = img.naturalHeight;
+        
+            // draw our image at position 0, 0 on the canvas
+            const ctx = outputImage.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+        
+            // show both the image and the canvas
+            document.body.appendChild(img);
+            document.body.appendChild(outputImage);
+        };
+        
+        // start loading our image
+
         img.src = imageUrl;
 
+
+
+
         // Print the image using print.js
-        printJS({ printable: img.src, type: 'image', imageStyle: 'max-width:100%;' });
+        printJS({
+            printable: img.src,
+            type: 'image',
+            imageStyle: `width:${window.width}; height:${window.height}; border-radius: 20px; object-fit: cover;`,
+          });
     } else {
         console.log("No file selected to print.");
     }
 }
+
+
+
 
 // Add a click event listener to the "Print" button
 document.getElementById("print-button").addEventListener("click", function () {
